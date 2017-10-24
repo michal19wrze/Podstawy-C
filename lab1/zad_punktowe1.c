e1ed770d75f933f49415854001e0e0d1f77c04f2
michal19wrze <mnogal@sigma.ug.edu.pl>
2017-10-24 23:48:39 +0200

    Poprawka

--- lab1/zad_punktowe1.c
+++ lab1/zad_punktowe1.c
@@ -5,7 +5,7 @@ int b;
 int c;
 int suma;
 int roznica;
-double iloczyn;
+int iloczyn;
 double iloraz;
 
 
@@ -15,18 +15,14 @@ printf("Podaj drugą liczbę!\n");
 scanf("%d",&b);
 printf("wybierz metode:\n 1.dodawanie\n 2.odejmowanie\n 3.mnożenie\n 4.dzielenie\n");
 scanf("%d",&c);
+
 suma=a+b;
 roznica=a-b;
 iloczyn=(double)a*b;
 iloraz=(double)a/b;
-if(c==1){
 
-
-  printf("Suma: %d\n",suma);
-}
+if(c==1){printf("Suma: %d\n",suma);}
 if(c==2){printf("Roznica: %d\n",roznica);}
-if(c==3){printf("Iloczyn: %f\n",iloczyn);}
+if(c==3){printf("Iloczyn: %d\n",iloczyn);}
 if(c==4){printf("Iloraz: %f\n",iloraz);}
-if(c==0){printf("Nie ma takiego działania\n");}
-else printf("Nie ma takiego działania\n");
 return 0;}
